63eff98a5d29a655ce67dbe25ad0c59c170f7343
enconpu <errorjiushierror@gmail.com>
2016-05-05 23:00:44 +0800

    2

--- src/filecontroller/Activator.java
+++ src/filecontroller/Activator.java
@@ -5,9 +5,8 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	/**
+	 * osgi start
 	 */
 	public void start(BundleContext context) throws Exception {
 		System.out.println("Hello World!!");
